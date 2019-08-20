4603e49b46723ba1e58d19865175f654ebec96d4
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-20 23:57:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,10 @@ end
 def hit?(card_total)
 
   prompt_user
-  get_user_input
-  if get_user_input == 's'
-      
-  elsif get_user_input == 'h'
+  user_input = get_user_input
+  if user_input == 's'
+    cart_total = card_total
+  elsif user_input == 'h'
     card_total += deal_card
   else
     invalid_command
