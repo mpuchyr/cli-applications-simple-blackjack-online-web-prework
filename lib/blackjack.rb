d9bbb6c139dce23dd3d2b55b719efa666d0caab1
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-20 23:55:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,19 +30,18 @@ def initial_round
 end
 
 def hit?(card_total)
-  user_input = "x"
-  
-  while user_input != "s" || user_input != "h"
-    prompt_user
-    user_input = get_user_input
-    if user_input == 's'
+
+  prompt_user
+  user_input = get_user_input
+  if user_input == 's'
       
-    elsif user_input == 'h'
-      card_total += deal_card
-    else
-      invalid_command
-    end
+  elsif user_input == 'h'
+    card_total += deal_card
+  else
+    invalid_command
+    hit?
   end
+
   
   card_total
 end
